85cbe34cebf01b88072f23425586d7c8b2a31fc4
kevinkjt2000 <kevinkjt2000@gmail.com>
2014-11-22 17:48:19 -0600

    Update ic2 api

---
+++ src/api/java/ic2/api/item/ILatheItem.java
@@ -0,0 +1,41 @@
+package ic2.api.item;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public interface ILatheItem {
+
+	/**
+	 * Returns the radius of this Lathe Item
+	 * Should be no more than 15 to still be able to display it properly.
+	 */
+	int getWidth(ItemStack stack);
+
+	/**
+	 * Returns the current state of this Lathe Item
+	 * The length of the array should be 5
+	 * The value should be the current size of the item at this time.
+	 * It should also not be greater than getWidth()
+	 */
+	int[] getCurrentState(ItemStack stack);
+
+	/**
+	 * This will set the current state of the Item on that position to that value.
+	 */
+	void setState(ItemStack stack, int position, int value);
+
+	/**
+	 * Returns the output Item (normally dust) when you lathe on this position.
+	 */
+	ItemStack getOutputItem(ItemStack stack, int position);
+
+	/**
+	 * Returns the ResourceLocation of the texture used to display the item inside of the Lathe GUI
+	 */
+	@SideOnly(Side.CLIENT)
+	ResourceLocation getTexture(ItemStack stack);
+
+}
